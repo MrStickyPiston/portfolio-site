1228ab6ad5e4edca8eaf0435c10550f1efed58d9
sticky <mr.sticky.piston@gmail.com>
2024-06-29 12:26:36 +0200

    started animation home testinominals

--- src/app/pages/home/testinominals/testinominals.component.ts
+++ src/app/pages/home/testinominals/testinominals.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -12,12 +13,39 @@ import { MatIconModule } from '@angular/material/icon';
     MatIconModule
   ],
   templateUrl: './testinominals.component.html',
-  styleUrl: './testinominals.component.scss'
+  styleUrl: './testinominals.component.scss',
+  animations: [
+    trigger('fadeInOut', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('600ms ease-in')
+      ]),
+      transition(':leave', [
+        animate('600ms ease-out', style({ opacity: 0 }))
+      ])
+    ]),
+    trigger('slideIn', [
+      transition(':enter', [
+        style({ transform: 'translateX(-100%)' }),
+        animate('300ms ease-in', style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate('300ms ease-out', style({ transform: 'translateX(-100%)' }))
+      ])
+    ])
+  ]
 })
 export class TestinominalsComponent {
   
   forward() {
-    throw new Error('Method not implemented.');
+    this.id ++;
+
+    if (this.id > this.items.length){
+      this.id = 0
+    }
+
+    this.item = this.items[this.id]
   }
   back() {
     throw new Error('Method not implemented.');
@@ -47,6 +75,22 @@ export class TestinominalsComponent {
                 inceptos himenaeos. Suspendisse volutpat tincidunt metus in pellentesque. Morbi pellentesque orci at
                 turpis
                 porttitor rutrum. Mauris rhoncus felis sed dictum viverra. Maecenas faucibus volutpat bibendum.`
+    },
+    {
+      name: "Fotoclub Borger-Odoorn",
+      description: ` Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a nisi in est fermentum rhoncus non
+                sed
+                sem. Aenean scelerisque nisi eget risus tristique, eget lacinia enim vulputate. Suspendisse potenti.
+                Nullam
+                condimentum quam vel nunc auctor mollis. Pellentesque blandit libero sit amet erat fermentum, vel
+                tincidunt
+                lorem volutpat. Curabitur tempus vel enim efficitur tristique. Morbi turpis ligula, faucibus in luctus
+                ut,
+                pretium iaculis arcu. Aliquam erat volutpat. Aenean ut tortor lacinia, ultricies libero blandit,
+                scelerisque
+                est. Etiam ex sem, interdum nec fermentum et, fringilla euismod velit.`
     }
   ]
+
+  item = this.items[this.id];
 }
