b54c16d058f51301a07cf1a27fd0432ee12f0a34
sticky <mr.sticky.piston@gmail.com>
2024-09-18 17:29:09 +0200

    site content

--- src/app/pages/projects/project-page/project-page.component.ts
+++ src/app/pages/projects/project-page/project-page.component.ts
@@ -58,11 +58,44 @@ export class ProjectPageComponent {
             id: 'about',
             subsections: [
               {
-                name: 'How it started',
+                name: 'Why i developed Sticky Hours',
                 items: [
                   {
                     type: ItemType.Text,
                     text: 'When i got to the upper part of secondary school i got a lot of gaps in my schedule. Not 3-4 much, but at least 7 and even more if lessons were cancelled. This inspired me to making this app, that allows you to quick and easily access see your gaps for this and coming weeks. But the best is that the app allows to add multiple users, and computes the common gaps between those users. Using this you can easily plan to work on a common project with your friends, move a lesson if your whole class has a common gap (resulting in a later start or an earlyer end), or just do something fun in those otherwise lost hours'
+                  }
+                ]
+              },
+            ]
+          },
+          {
+            name: 'Features',
+            id: 'features',
+            subsections: [
+              {
+                name: 'Finding gaps in schedules'
+              },
+              {
+                name: 'Always up to date schedule data'
+              },
+              {
+                name: 'Computing common gaps'
+              },
+              {
+                name: 'Cross platform support'
+              },
+              {
+                name: 'Easy to use interface',
+                items: [
+                  {
+                    type: ItemType.Images,
+                    images: [{
+                      url: 'StickyHours-screenshot-common_hours_menu',
+                      alt: 'Sticky Hours android app icon, used as banner for the project page',
+                      width: 3120,
+                      height: 1440,
+                    }
+                  ]
                   }
                 ]
               },
@@ -74,19 +107,7 @@ export class ProjectPageComponent {
                     text: 'Open source helps improve the app as anyone can contribute fixes and features.'
                   },
                   {
-                    type: ItemType.Images,
-                    images: [{
-                      url: 'StickyHours-screenshot-common_hours_menu',
-                      alt: 'Sticky Hours android app icon, used as banner for the project page',
-                      width: 3120,
-                      height: 1440,
-                    },
-                    {
-                      url: 'StickyHours-screenshot-common_hours_menu',
-                      alt: 'Sticky Hours android app icon, used as banner for the project page',
-                      width: 3120,
-                      height: 1440,
-                    }]
+                    type: ItemType.Buttons
                   }
                 ]
               }
